18bf19c2fda9d6ebf52937c9619266dd9fa1954d
Alex Hoyau <a.hoyau@silexlabs.org>
2018-06-07 19:05:05 +0200

    env var for heroku mongo

--- app/env.js
+++ app/env.js
@@ -4,7 +4,7 @@ const env = {
   nodeEnv: process.env.NODE_ENV || 'production',
   port: process.env.PORT || 3000,
   sessionSecret: process.env.SESSION_SECRET || 'please set the SESSION_SECRET env var',
-  mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/fbwm',
+  mongoUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/fbwm',
   fbAppSecret: process.env.FB_APP_SECRET,
   fbTabPageAppSecret: process.env.FB_TAB_PAGE_APP_SECRET,
 };
